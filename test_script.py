710ad6bb725e8a46d59dcb48253f04a304175896
Adian Liusie <adian@al826f.pem.private.cam.ac.uk>
2019-05-17 21:22:50 +0100

    slight update to allow for input of food, weight, and type of food

--- test_script.py
+++ test_script.py
@@ -1,12 +1,16 @@
 import pandas
 
-x = 12
+weight = 100
+weight /= 100
+fct_number = 12
+conv_number = 431
+
 fct = pandas.read_csv('Fct.csv', encoding='utf-8')
-fct_row = fct[fct["C_CODE                   "] == x]
+fct_row = fct[fct["C_CODE                   "] == fct_number]
 print(fct_row)
 
 conv = pandas.read_csv('RetentionFactors.csv', encoding='utf-8')
-conv_row = conv[conv["R_Code"]==431]
+conv_row = conv[conv["R_Code"]==conv_number]
 print(conv_row)
 
 transpose_conv = conv_row.T
@@ -15,4 +19,4 @@ transpose_fct = fct_row.T
 fct_array = transpose_fct.iloc[3:].values.T[0]
 conv_array = transpose_conv.iloc[2:].values.T[0]
 
-print(conv_array)
+print(weight*conv_array*fct_array)
