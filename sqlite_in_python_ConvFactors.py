eb945f98cca7d52ad6748fffe45d7db57759b277
Junxiao Shen <junxiaoshen@js2283.trin.private.cam.ac.uk>
2019-05-18 16:11:25 +0100

    new
    Co-Authored-By: shawnshenjx <shawnshenjx@users.noreply.github.com>

--- sqlite_in_python_ConvFactors.py
+++ sqlite_in_python_ConvFactors.py
@@ -13,11 +13,11 @@ db = sqlite3.connect('icrisat.db')
 cursor = db.cursor()
 
 cursor.execute('''
-    create table ConvFactors(conv_codetype,conv_foodcode,conv_food_descr,conv_method,conv_method_descr,conv_stdvol,conv_stdvol_descr,conv_size,conv_size_descr,conv_factor,conv_unit,conv_unit_descr,conv_nobs,conv_source,conv_source_descr,,,,source2 (Mourad))
+    create table ConvFactors(conv_codetype,conv_foodcode,conv_food_descr,conv_method,conv_method_descr,conv_stdvol,conv_stdvol_descr,conv_size,conv_size_descr,conv_factor,conv_unit,conv_unit_descr,conv_nobs,conv_source,conv_source_descr)
 ''')
 
 
-df = pd.read_csv(r'/Users/junxiaoshen/Documents/GitHub/ICRISAT-backend/ConvFactors.csv')
+df = pd.read_csv(r'/Users/junxiaoshen/Documents/GitHub/ICRISAT-backend/ConvFactors1.csv')
 df.to_sql('ConvFactors', db, if_exists='append', index=False)
 
 db.commit()
